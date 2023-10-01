ad5fd1883e9484709d0380e72bee43e7be728ca1
hdvtec <hdvtec@gmail.com>
2023-10-01 11:21:12 +0100

    Version 1

--- Dir_Sync/Program.cs
+++ Dir_Sync/Program.cs
@@ -2,46 +2,99 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+
 
 namespace Dir_Sync
 {
     internal class Program
     {
+        /// <summary>
+        /// Main Program
+        /// </summary>
+        /// <param name="args">
+        /// [0] - Origin Directory
+        /// [1] - Destination Directory
+        /// [2] - Log File
+        /// [3] - Period to repeat (seconds)
+        /// </param>
         static void Main(string[] args)
         {
-            string dir1 = args[0];
-            string dir2 = args[1];
+            //Run Peridically
+            //Exception Test/Handle
 
-            var result1 = Get(dir1);
-            var result2 = Get(dir2);
+            try
+            {
+                //--- Check If Log Can Be created
+                string logPath = Path.GetDirectoryName(args[2]);
+                if (!Directory.Exists(logPath))
+                {
+                    Directory.CreateDirectory(logPath);
+                    if (!Directory.Exists(logPath))
+                    {
+                        Console.WriteLine("Unable to create log file path.");
+                        Console.WriteLine("Press enter to close...");
+                        Console.ReadLine();
+                    }
+                }
 
-            Console.WriteLine(args[0]);
-            Console.WriteLine(args[1]);
+                //Periodically run should be done in Windows Task Schedule
+                while (true)
+                {
+                    var result1 = GetContent(args[0]);
+                    var result2 = GetContent(args[1]);
 
-            var dirsToRemove = result2.dirs.Where(x => !result1.dirs.Contains(x));
-            var dirsToAdd = result1.dirs.Where(x=> !result2.dirs.Contains(x));
-            var filesToRemove = result2.files.Where(x => !result1.files.Contains(x));
-            var filesToAdd = result1.files.Where(x => !result2.files.Contains(x));
+                    var dirsToRemove = result2.dirs.Where(x => !result1.dirs.Contains(x));
+                    var dirsToAdd = result1.dirs.Where(x => !result2.dirs.Contains(x));
+                    var filesToRemove = result2.files.Where(x => !result1.files.Contains(x));
+                    var filesToAdd = result1.files.Where(x => !result2.files.Contains(x));
+                    var filesToCompare = result1.files.Where(x => result2.files.Contains(x));
 
-            foreach (var fileToRemove in filesToRemove)
-                Console.WriteLine("Deleting : " + Path.Combine(dir2, fileToRemove.file));
+                    foreach (var fileToRemove in filesToRemove)
+                    {
+                        Console.WriteLine(DateTime.Now.ToString() +  " - Deleting : " + Path.Combine(args[1], fileToRemove.file));
+                        using (StreamWriter sw = new StreamWriter(args[2], true))
+                            sw.WriteLine(DateTime.Now.ToString() + " - Deleting : " + Path.Combine(args[1], fileToRemove.file));
+                        File.Delete(Path.Combine(args[1], fileToRemove.file));
+                    }
 
-            foreach (var dirToRemove in dirsToRemove)
-                Console.WriteLine("Deleting : " + Path.Combine(dir2, dirToRemove));
+                    foreach (var dirToRemove in dirsToRemove)
+                    {
+                        Console.WriteLine(DateTime.Now.ToString() + " - Deleting : " + Path.Combine(args[1], dirToRemove));
+                        using (StreamWriter sw = new StreamWriter(args[2], true))
+                            sw.WriteLine(DateTime.Now.ToString() + " - Deleting : " + Path.Combine(args[1], dirToRemove));
+                        Directory.Delete(Path.Combine(args[1], dirToRemove));
+                    }
 
-            foreach (var dirToAdd in dirsToAdd)
-                Console.WriteLine("Adding : " + Path.Combine(dir2, dirToAdd));
+                    foreach (var dirToAdd in dirsToAdd)
+                    {
+                        Console.WriteLine(DateTime.Now.ToString() + " - Adding : " + Path.Combine(args[1], dirToAdd));
+                        using (StreamWriter sw = new StreamWriter(args[2], true))
+                            sw.WriteLine(DateTime.Now.ToString() + " - Adding : " + Path.Combine(args[1], dirToAdd));
+                        Directory.CreateDirectory(Path.Combine(args[1], dirToAdd));
+                    }
+
+                    foreach (var fileToAdd in filesToAdd)
+                    {
+                        Console.WriteLine(DateTime.Now.ToString() + " - Adding : " + Path.Combine(args[1], fileToAdd.file));
+                        using (StreamWriter sw = new StreamWriter(args[2], true))
+                            sw.WriteLine(DateTime.Now.ToString() + " - Adding : " + Path.Combine(args[1], fileToAdd.file));
+                        File.Copy(Path.Combine(args[0], fileToAdd.file), Path.Combine(args[1], fileToAdd.file));
+                    }
+
+                    System.Threading.Thread.Sleep(Convert.ToInt32(args[3]) * 1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Press enter to close...");
+                Console.ReadLine();
+            }
             
-            foreach (var fileToAdd in filesToAdd)
-                Console.WriteLine("Adding : " + Path.Combine(dir2, fileToAdd.file));
-
-            Console.WriteLine("Press enter to close...");
-            Console.ReadLine();
         }
 
-        public static (HashSet<string> dirs, HashSet<(string file, long tick)> files) Get(string dir)
+        public static (HashSet<string> dirs, HashSet<(string file, long tick)> files) GetContent(string dir)
         {
             string CleanPath(string path) => path.Substring(dir.Length).TrimStart('/', '\\');
 
@@ -49,7 +102,7 @@ namespace Dir_Sync
             long GetUnique(string path) => new FileInfo(path).LastWriteTime.Ticks;
 
             var directories = Directory.GetDirectories(dir, "*.*", SearchOption.AllDirectories);
-            directories = AddElementAtBeggining(dir, directories);
+            directories = AddElementAtBegin(dir, directories);
 
             var dirHash = directories.Select(CleanPath).ToHashSet();
 
@@ -61,7 +114,7 @@ namespace Dir_Sync
             return (dirHash, fileHash);
         }
 
-        private static string[] AddElementAtBeggining(string dir, string[] directories)
+        private static string[] AddElementAtBegin(string dir, string[] directories)
         {
             int newLength = directories.Length + 1;
 
